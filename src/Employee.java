a1fe36fddc20deadf148605f557123df95312fa5
anastasiya.nedolivko <anastasiya.nedolivko@ringteam.com>
2024-10-20 22:50:50 +0300

    Employees

--- src/Employee.java
+++ src/Employee.java
@@ -1,36 +1,47 @@
-public class Employee {
-    // TODO fix class declaration and declare variables here
+public class Employee implements Payable {
+    private String employeeId;
+    private String name;
 
     public Employee(String employeeId, String name) {
-        // TODO fill in code here
+        this.employeeId = employeeId;
+        this.name = name;
+    }
+
+    public Employee() {
     }
 
     public String getEmployeeId() {
-        // TODO fill in code here and replace the return statement
-        return "";
+        return employeeId;
     }
 
     public void setEmployeeId(String employeeId) {
-        // TODO fill in code here
+        this.employeeId = employeeId;
     }
 
     public String getName() {
-        // TODO fill in code here and replace the return statement
-        return "";
+        return name;
     }
 
     public void setName(String name) {
-        // TODO fill in code here
+        this.name = name;
     }
 
     public double getAverageMonthlySalary() {
-        // TODO fill in code here and replace the return statement
-        return 0;
+        return calculatePay();
     }
 
     @Override
     public String toString() {
-        // TODO fill in code here and replace the return statement, be sure to format double value
-        return "";
+        String roundedSalary = String.format("%.2f", (double)Math.round(getAverageMonthlySalary()));
+        return "Employee{" +
+                "employeeId='" + employeeId + '\'' +
+                ", name='" + name + '\'' +
+                ", average monthly salary='" + roundedSalary + '\'' +
+                '}';
+    }
+
+    @Override
+    public double calculatePay() {
+        return 0;
     }
 }
