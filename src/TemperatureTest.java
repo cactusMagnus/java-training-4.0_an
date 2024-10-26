e5a9074fcd05cbc1e70d6a6b69eb13bd2d2313aa
anastasiya.nedolivko <anastasiya.nedolivko@ringteam.com>
2024-10-26 12:28:29 +0300

    Temperatures

--- src/TemperatureTest.java
+++ src/TemperatureTest.java
@@ -14,8 +14,9 @@ public class TemperatureTest {
         return new Object[][]{
                 {212.0, 100.0},
                 {32.0, 0.0},
-                {-15, -26.11111111111111}
-                // TODO add 2 more test data here
+                {-15, -26.11111111111111},
+                {104.0, 40.0},
+                {-166.0, -110.0}
         };
     }
 
@@ -28,8 +29,9 @@ public class TemperatureTest {
         return new Object[][]{
                 {100.0, 212.0},
                 {0.0, 32.0},
-                {-26.11111111111111, -15}
-                // TODO add 2 more test data here
+                {-26.11111111111111, -15},
+                {58.0, 136.4},
+                {12.5, 54.5}
         };
     }
 
